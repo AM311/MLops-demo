719a1f3d712785b5e8c31213a944c4f34510a6fe
Alessio Manià <62076703+AM311@users.noreply.github.com>
2024-11-29 13:00:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.set_page_config(page_title="MLOps - Demo",
+                   page_icon=None,
+                   layout="centered",
+                   initial_sidebar_state="auto",
+                   menu_items=None)
+
+st.title("MLOps - Demo")
+st.header(":blue[Main Page]")
+st.markdown("# Test Main Page")
+st.markdown("## Test subtitle!")
+st.markdown("Lorem **ipsum** dolor *sit amet*")
